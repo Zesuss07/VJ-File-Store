da3ca747a8e34280b713cad8255905e29c473697
Mr Frontman <mohittatu2007@gmail.com>
2025-07-27 04:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "22422690"))
 API_HASH = environ.get("API_HASH", "2884fbc53b44a1bbcfa7c525e185c1aa")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://files.catbox.moe/asmdx2.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7793257011').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Getyourlink01bot") # without @
 PORT = environ.get("PORT", "8080")
